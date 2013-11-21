2ccdd9f8d3762f20d39b3110db5b9aeaf6f52101
Johooh <reimann.johan@gmail.com>
2013-11-21 14:15:00 +0100

    fordelat koden lite

--- database.js
+++ database.js
@@ -8,7 +8,6 @@ $.getJSON("database.php", function(data){
 alert(hej);
 });
 
-
 	 simpleText = {
 					      x: (window.innerHeight/100)*20,
 					      y: (window.innerHeight/100)*90,
@@ -19,4 +18,121 @@ alert(hej);
 					       
 					};
 
+	var rect_meny_layout = {
+		x: percent_width*50,
+		y: percent_height*46,
+		width: percent_width*50,
+		height: percent_height*40,
+		fill: 'white'
 
+	};
+
+	var text_quest_meny_layout = {
+    	x: percent_width*30,
+    	y: percent_height*30,
+    	text: 'MENY',
+    	fontSize: 50,
+    	fontFamily: 'Calibri',
+    	fill: 'black'
+    };
+
+    var rect_start_btn_layout =  {
+      	x: percent_width*50,
+      	y: percent_height*50,
+      	width: percent_width*10,
+      	height: percent_height*10,
+      	fill: 'lightblue',
+  	};
+
+  	var text_quest_start_btn_layout = {
+    	x: percent_width*50,
+    	y: percent_height*50,
+    	text:'Start',
+    	fontSize: 30,
+    	fontFamily: 'Calibri',
+    	fill: 'black' 
+    };
+
+    var circleAnswer_layout = {
+    	 x: ((window.innerWidth)/1920)*1108.6,
+    	 y: ((window.innerHeight)/979)*351.6,
+         radius: 1,
+         fill: 'red',
+         stroke: 'white',
+         strokeWidth: 2,
+         dragbounds: {
+    		left: stage.getWidth(), right: stage.getHeight(),
+        },
+        visible: false,
+    };
+
+	//Skapar variabel med cirkelns egenskaper 
+    //x: bestämmer startposition i x
+    //y: bestämmer startposition i y
+    //Radius: bestämmer radien
+    //Fill: bestämmer innerfärgen
+    //Stroke: bestämmer randfärgen
+    //Strokewidth: randstorlek
+    //draggable: true/false
+    //dragbounds: Vart man får dra den
+    var circleCenter_layout = {
+    	x: percent_width*50,
+    	y: percent_height*50,
+    	radius: 5,
+    	fill: 'red',
+    	stroke: 'white',
+    	strokeWidth: 2,
+    	draggable: true,
+    	dragbounds: {
+    		left: stage.getWidth(), right: stage.getHeight()
+    	}
+    };
+
+    var text_quest_layout = {
+		x: percent_width*20,
+		y: percent_height*91,
+		text: 'Författaren till klassikern "Brott och straff" är född \n i hans fosterlands huvudstad, vart ligger denna huvudstad?',
+		fontSize: percent_height*3,
+		fontFamily: 'Calibri',
+		fill: 'white'     
+		};
+
+	var text_ans_layout{
+          x: percent_width*7,
+	      y: percent_height*92,
+	      text: 'Moskva',
+          fontSize: percent_height*3,
+          fontFamily: 'Calibri',
+	      fill: 'darkgray',
+	      visible: false    
+   };
+
+	var rect_next_layout = {
+		x: percent_width*85,
+		y: percent_height*94,
+		width: percent_width*7,
+		height: percent_height*7,
+		fill: 'lightblue',
+		visible: false
+	};
+
+	var text_next_btn_layout = {
+          x: percent_width*85,
+	      y: percent_height*94,
+	      text: 'Nästa',
+          fontSize: percent_height*3,
+          fontFamily: 'Calibri',
+	      fill: 'black',
+	      visible: false				       
+   };
+
+   var rect_bar_layout = {
+         x: 0,
+         y: percent_height*89,
+         width: percent_width*100,
+         height: h2/8,
+         fill: 'black',
+         stroke: 'black',
+         strokeWidth: 4,
+         opacity: 0.5
+  };
